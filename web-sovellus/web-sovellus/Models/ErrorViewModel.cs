9a4264db6644701e6d9021bb9a8f6dc53be94aeb
Valtteri Korhonen <Valtteri.Korhonen3@edu.savonia.fi>
2019-09-05 10:25:43 +0300

    Lisää vs projekti

---
+++ web-sovellus/web-sovellus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_sovellus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
