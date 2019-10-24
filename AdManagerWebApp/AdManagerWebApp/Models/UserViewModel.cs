bedeaf070d7bd955653ee907cb269d3516d308de
Valtteri Korhonen <Valtteri.Korhonen3@edu.savonia.fi>
2019-10-24 08:28:31 +0300

    add id

--- AdManagerWebApp/AdManagerWebApp/Models/UserViewModel.cs
+++ AdManagerWebApp/AdManagerWebApp/Models/UserViewModel.cs
@@ -7,6 +7,7 @@ namespace AdManagerWebApp.Models
 {
     public class UserViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string DisplayName { get; set; }
